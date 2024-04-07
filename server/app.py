a3d3c1d67f51ef8de44dddd589f1adbb975841ad
Xennia Contreras <Xenbydesign@gmail.com>
2024-04-06 19:52:52 -0700

    done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, g
 from flask_restful import Api, Resource
 import os
 
@@ -20,9 +20,86 @@ db.init_app(app)
 api = Api(app)
 
 
+@app.before_request
+def before_request():
+    if request.endpoint == "restaurantsbyid":
+        id = request.view_args.get("id")
+        rest = db.session.get(Restaurant, id)
+        g.rest = rest
+
+
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+
+class Restaurants(Resource):
+    def get(self):
+        try:
+            restaurants = [
+                restaurant.to_dict(rules=("-restaurant_pizzas",))
+                for restaurant in Restaurant.query
+            ]
+            return restaurants, 200
+        except Exception as e:
+            return {"error": str(e)}, 400
+
+
+class RestaurantsById(Resource):
+    def get(self, id):
+        try:
+            # if restaurant := db.session.get(Restaurant, id):
+            # return restaurant.to_dict(), 200
+            if g.rest:
+                return g.rest.to_dict(), 200
+            else:
+                return {"error": "Restaurant not found"}, 404
+        except Exception as e:
+            return {"error": str(e)}, 400
+
+    def delete(self, id):
+        try:
+            # if restaurant := db.session.get(Restaurant, id):
+            #     db.session.delete(restaurant)
+            if g.rest:
+                db.session.delete(g.rest)
+                db.session.commit()
+                return {}, 204
+            else:
+                return {"error": "Restaurant not found"}, 404
+        except Exception as e:
+            return {"error": str(e)}, 400
+
+
+class Pizzas(Resource):
+    def get(self):
+        try:
+            pizzas = [
+                pizza.to_dict(rules=("-restaurant_pizzas",)) for pizza in Pizza.query
+            ]
+            return pizzas, 200
+        except Exception as e:
+            return {"error": str(e)}, 400
+
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            new_pizza = RestaurantPizza(**data)
+            db.session.add(new_pizza)
+            db.session.commit()
+            return new_pizza.to_dict(), 201
+        except Exception as e:
+            db.session.rollback()
+            # return {"error": str(e)}, 400
+            return {"errors": [str(e)]}, 400
+
+
+api.add_resource(Restaurants, "/restaurants")
+api.add_resource(Pizzas, "/pizzas")
+api.add_resource(RestaurantsById, "/restaurants/<int:id>")
+api.add_resource(RestaurantPizzas, "/restaurant_pizzas")
 
 
 if __name__ == "__main__":
---
+++ server/migrations/versions/1d23a3985bc6_updated_space_issue.py
@@ -0,0 +1,24 @@
+"""updated space issue
+
+Revision ID: 1d23a3985bc6
+Revises: b26c97879f88
+Create Date: 2024-04-06 17:09:53.115750
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1d23a3985bc6'
+down_revision = 'b26c97879f88'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
